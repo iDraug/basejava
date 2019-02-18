a3d400e583e5143987cec4ad87e224eb85fbf4e0
iDraug <draygwolf@gmail.com>
2019-02-18 21:55:23 +0500

    Homework is done.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,28 +3,49 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int count = 0;
 
     void clear() {
+        for (int i = 0; i < count; i++) {
+            storage[i] = null;
+        }
+        count = 0;
     }
 
     void save(Resume r) {
+        storage[count] = r;
+        count++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < count; i++) {
+            if (storage[i].uuid.equals(uuid)) return storage[i];
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < count; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                for (int n = i; n < count; n++) {
+                    storage[n] = storage[n + 1];
+                    storage[n + 1] = null;
+                }
+                count--;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] storage2 = new Resume[count];
+        for (int i = 0; i < count; i++) storage2[i] = storage[i];
+        return storage2;
     }
 
     int size() {
-        return 0;
+        return count;
     }
 }
