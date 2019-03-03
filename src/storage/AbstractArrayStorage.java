41f1afbc70c4acf3aab7e7128dbaf3a9bb9c5074
iDraug <draygwolf@gmail.com>
2019-03-03 22:47:52 +0500

    HW3 | fixed "delete", "save" methods in SortedArraySorage, add Override annotation

--- src/storage/AbstractArrayStorage.java
+++ src/storage/AbstractArrayStorage.java
@@ -5,7 +5,7 @@ import model.Resume;
 import java.util.Arrays;
 
 public abstract class AbstractArrayStorage implements Storage {
-    protected static final int STORAGE_LIMIT = 2;
+    protected static final int STORAGE_LIMIT = 10_000;
 
     protected Resume[] storage = new Resume[STORAGE_LIMIT];
     protected int size = 0;
